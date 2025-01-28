79c36cc9d8fc03f38b9850d71e9431a19fc4fea1
city118 <cityckc@qq.com>
2025-01-28 22:13:08 +0800

    登录-axios请求头注入Token

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,5 +1,14 @@
-
+import { request } from "@/utils"
+import { useEffect } from "react"
 export default function Layout() {
+
+  useEffect(() => {
+    // 请求用户信息
+    request.get('/user/profile').then(res => {
+      console.log(res);
+    })
+  }, [])
+
   return (
     <div>首页</div>
   )
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getToken } from "../utils";
 
 // 创建axios实例
 const request = axios.create({
@@ -9,6 +10,12 @@ const request = axios.create({
 // 请求拦截器
 // 在请求发送之前做拦截，插入一些自定义的配置
 request.interceptors.request.use(config => {
+        // 注入token
+        const token = getToken();
+        if(token){
+            // 将token添加到请求头中
+            config.headers.Authorization = `Bearer ${token}`
+        }
         return config
     },
     err => Promise.reject(err)
