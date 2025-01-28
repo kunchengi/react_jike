191664bd1f7a83c0764d0682fb408e18ec074232
city118 <cityckc@qq.com>
2025-01-28 17:45:49 +0800

    登录-准备静态结构

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { Card, Form, Input, Button } from 'antd';
+import logo from '@/assets/logo.png'
+import './index.scss'
 export default function Login() {
     return (
-        <div>
-            登录页面
+        <div className='login'>
+            {/* 卡片 */}
+            <Card className='login-container'>
+                <img className='login-logo' alt='logo' src={logo} />
+                {/* 登录表单 */}
+                <Form>
+                    <Form.Item>
+                        <Input size='large' placeholder='请输入手机号' />
+                    </Form.Item>
+                    <Form.Item>
+                        <Input size='large' placeholder='请输入验证码' />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type='primary' size='large' htmlType='submit' block>登录</Button>
+                    </Form.Item>
+                </Form>
+            </Card>
         </div>
     )
 }
