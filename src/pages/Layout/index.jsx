e324a5e03870427c39564d25d78d10c7a0c23834
city118 <cityckc@qq.com>
2025-02-01 14:38:24 +0800

    搭建首页的静态结构

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,15 +1,59 @@
-import { request } from "@/utils"
-import { useEffect } from "react"
-export default function Layout() {
+import { Layout, Menu, Popconfirm } from "antd"
+import {
+  HomeOutlined,
+  DiffOutlined,
+  EditOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons"
+import './index.scss'
 
-  useEffect(() => {
-    // 请求用户信息
-    request.get('/user/profile').then(res => {
-      console.log(res);
-    })
-  }, [])
+const { Header, Sider } = Layout
+
+const items = [
+  {
+    label: '首页',
+    key: '/',
+    icon: <HomeOutlined />
+  },
+  {
+    label: '文章管理',
+    key: '/article',
+    icon: <DiffOutlined />
+  },
+  {
+    label: '创建文章',
+    key: '/publish',
+    icon: <EditOutlined />
+  }
+]
+export default function GeekLayout() {
 
   return (
-    <div>首页</div>
+    <Layout className="ant-layout">
+      <Header className="hander">
+        <div className='logo' />
+        <div className="user-info">
+          <span className="user-name">ccc</span>
+          <span className="user-logout">
+            <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消">
+              <LogoutOutlined /> 退出
+            </Popconfirm>
+          </span>
+        </div>
+      </Header>
+      <Layout>
+        <Sider width={200} className="site-layout-background">
+          <Menu
+            mode="inline"
+            theme="dark"
+            defaultSelectedKeys={['1']}
+            items={items}
+            style={{ height: '100%', borderRight: 0 }}></Menu>
+        </Sider>
+        <Layout className="layout-content" style={{ padding: 20 }}>
+          内容
+        </Layout>
+      </Layout>
+    </Layout>
   )
 }
