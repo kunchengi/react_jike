740696d309c7c3d4fb4838e6cf912300f2430ab0
Kenson <3049425262@qq.com>
2025-02-26 22:23:17 +0800

    发布文章-收集表单数据并调用发布文章接口

--- src/apis/article.js
+++ src/apis/article.js
@@ -8,3 +8,13 @@ export function getChannelAPI() {
         method: 'GET'
     })
 }
+
+// 新建文章
+export function createArticleAPI(articleData) {
+    return request({
+        // draft: false 表示发布文章，true表示存为草稿
+        url: '/mp/articles?draft=false',
+        method: 'POST',
+        data: articleData
+    })
+}
--- src/pages/Layout/Publish/index.jsx
+++ src/pages/Layout/Publish/index.jsx
@@ -1,10 +1,10 @@
-import { Card, Form, Input, Button, Breadcrumb, Select, Space } from "antd"
+import { Card, Form, Input, Button, Breadcrumb, Select, Space, message } from "antd"
 import { Link } from "react-router-dom"
 // 导入富文本编辑器包
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import { useEffect, useState } from "react";
-import { getChannelAPI } from "@/apis/article";
+import { useEffect, useRef, useState } from "react";
+import { getChannelAPI, createArticleAPI } from "@/apis/article";
 import './index.scss'
 export default function Publish() {
 
@@ -22,13 +22,36 @@ export default function Publish() {
         getChannels();
     }, []);
 
+    // 表单提交
+    const onFinish = async (formValue) => {
+        // 按照接口文档的格式处理收集到的表单数据
+        const { title, channel_id, content } = formValue;
+        const articleData = {
+            title,
+            content,
+            // 封面，暂时没有上传图片，所以图片数组为空
+            cover: {
+                type: 0,
+                images: []
+            },
+            channel_id
+        };
+        const sussess = await createArticleAPI(articleData);
+        if(sussess?.message === 'OK')
+        {
+            message.success('发布成功');
+        }
+    }
+
+    const quillRef = useRef(null);
+
     return (
         <div className="publish">
             <Card title={<Breadcrumb items={[
                 { title: <Link to="/">首页</ Link> },
                 { title: "发布文章" }
             ]} />}>
-                <Form labelCol={{ span: 4 }} wrapperCol={{ span: 16 }} initialValues={{ type: 1 }}>
+                <Form labelCol={{ span: 4 }} wrapperCol={{ span: 16 }} initialValues={{ type: 1 }} onFinish={onFinish}>
                     <Form.Item label="标题" name="title" rules={[{ required: false, message: '请输入文章标题' }]}>
                         <Input placeholder="请输入文章标题" style={{ width: 400 }} />
                     </Form.Item>
@@ -39,12 +62,12 @@ export default function Publish() {
                         >
                             {/* 循环遍历渲染频道列表 */}
                             {/* value属性用户选中后会自动收集起来，作为接口的提交字段 */}
-                            {channelList.map(item => <option key={item.id} value={item.id}>{item.name}</option>)}
+                            {channelList.map(item => <Select.Option key={item.id} value={item.id}>{item.name}</Select.Option>)}
                         </Select>
                     </Form.Item>
                     <Form.Item label="内容" name="content" rules={[{ required: true, message: '请输入文章内容' }]}>
                         {/* 富文本编辑器 */}
-                        <ReactQuill className="publish-quill" theme="snow" placeholder="请输入文章内容" />
+                        <ReactQuill ref={quillRef} className="publish-quill" theme="snow" placeholder="请输入文章内容" />
                     </Form.Item>
                     <Form.Item wrapperCol={{ offset: 4 }}>
                         <Space>
